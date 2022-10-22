2120d666043ff59a038a9a75200175530efa8fb5
sun <11177827+a3434817612@user.noreply.gitee.com>
2022-10-22 21:52:57 +0800

    添加vo类

---
+++ src/main/java/com/smallfatcat/vo/response/UserResponseVo.java
@@ -0,0 +1,28 @@
+package com.smallfatcat.vo.response;
+
+import java.io.Serializable;
+
+public class UserResponseVo implements Serializable {
+    //姓名
+    private String name;
+
+
+    //手机号
+    private String phone;
+
+
+    //性别 0 女 1 男
+    private String sex;
+
+
+    //身份证号
+    private String idNumber;
+
+
+    //头像
+    private String avatar;
+
+
+    //状态 0:禁用，1:正常
+    private Integer status;
+}
